8b925cf275c43c9e1fe5983cf47d17be06cb608f
Jennifer Tran <jennifer.tran@Jennifer-Trans-MacBook-Pro.local>
2024-01-10 17:35:24 -0800

    chore: unmerged files

--- src/components/AddressCard.tsx
+++ src/components/AddressCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ClaimButton from './ClaimButton';
 
 interface AddressCardProps {
     ethereum_address: string;
@@ -7,20 +8,25 @@ interface AddressCardProps {
 
 export default function AddressCard({ ethereum_address, listOfAddresses }: AddressCardProps) {
     return (
-        <div className="card w-full bg-primary text-primary-content">
-            <div className="card w-1/2 bg-primary text-primary-content">
+        <div className="card w-full bg-primary text-primary-content flex flex-col">
+            <div className="card w-full bg-primary text-primary-content">
                 <div className="card-body">
-                    <h2 className="card-title">Cookie Jar Created!</h2>
-                    <p>Ethereum Address: {ethereum_address}</p>
+                    <h2 className="card-title mb-4">Cookie Jar Created!</h2>
+                    <div className="mb-2">
+                        <span className="font-bold">Ethereum Address:</span><p>{ethereum_address}</p>
+                    </div>
                     <div>
-                        <p>Addresses that can take from the jar:</p>
+                        <p className="font-bold">Addresses that can take from the jar:</p>
                         {listOfAddresses.map((address, index) => (
-                            <div key={index}>
-                                <p className="card-body">{address}</p>
-                            </div>
+                            <li key={index}>
+                                {address.toLowerCase()}
+                            </li>
                         ))}
                     </div>
                 </div>
+            </div>
+            <div className="flex flex-col w-1/2 pr-5 ml-auto mb-4">
+                <ClaimButton />
             </div>
         </div>
     );
